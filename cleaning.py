5d555c44ad483053178ce909a65dd5d465793350
SarahMBF <sara.maged555@gmail.com>
2025-05-20 22:26:57 +0200

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,5 @@
+# We will create a function called "clean_clnt_age" to dropp any row with a NaN value for the column "clnt_age" and apply it to the df "df_final_demo".
+
+def clean_clnt_aged(df)
+# We will clean any rows containing "9999999900" at the end of their value for "bal" with a function.
+def clean_balance(x)
